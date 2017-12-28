849d1af42e874b908c7be439a7728a7d2866c266
cktang88 <arctangent20@yahoo.com>
2017-12-27 21:05:58 -0600

    Add documentation

--- public/domhelpers.js
+++ public/domhelpers.js
@@ -1,8 +1,14 @@
-const $ = (str) => { // jquery syntax shortcuts
+/*
+  * mimic jQuery DOM functionality with jQuery syntax
+  * creates elements, and selects elements using $
+  *
+  */
+
+const $ = (str) => {
   const len = str.length;
   switch (str[0]) {
     case '<': // create dom element
-      if (str[len - 1] == '>') { return document.createElement(str.slice(1, len - 1)); }
+      if (str[len - 1] === '>') { return document.createElement(str.slice(1, len - 1)); }
       break;
     default: // select dom element
       const nodes = document.querySelectorAll(str);
